aa643bc1f223a39d0b43070d23f4b872fd78b0f9
aoiorio <130330538+aoiorio@users.noreply.github.com>
2025-06-07 14:11:38 +0900

    delete(code): unused codes as in sqs and the policy for it

--- main.tf
+++ main.tf
@@ -2,46 +2,10 @@ resource "aws_sns_topic" "user_updates" {
   name = "user-updates-topic"
 }
 
-resource "aws_sqs_queue" "user_updates_queue" {
-  name   = "user-updates-queue"
-  policy = data.aws_iam_policy_document.sqs_queue_policy.json
-}
-
 resource "aws_sns_topic_subscription" "user_updates_queue" {
   topic_arn = aws_sns_topic.user_updates.arn
   protocol  = "email"
   endpoint  = var.private.email
-}
-
-data "aws_iam_policy_document" "sqs_queue_policy" {
-  policy_id = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
-
-  statement {
-    sid    = "user_updates_sqs_target"
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["sns.amazonaws.com"]
-    }
-
-    actions = [
-      "SQS:SendMessage",
-    ]
-
-    resources = [
-      "arn:aws:iam::aws:policy/AmazonSQSFullAccess",
-    ]
-
-    condition {
-      test     = "ArnEquals"
-      variable = "aws:SourceArn"
-
-      values = [
-        aws_sns_topic.user_updates.arn,
-      ]
-    }
-  }
 }
 
 resource "aws_budgets_budget" "cost" {
