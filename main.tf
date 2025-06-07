9b2af627ecfd322df78cc07e49e884807d49246d
aoiorio <130330538+aoiorio@users.noreply.github.com>
2025-06-07 12:17:08 +0900

    feat(terraform): create a budget plan and a topic subscription as well as sqs by using terraform

--- main.tf
+++ main.tf
@@ -1,34 +1,64 @@
-# billing
-variable "billing_name" {
-    default = "you-are-running-out-of-money"
+resource "aws_sns_topic" "user_updates" {
+  name = "user-updates-topic"
 }
 
-variable "billing_threshold" {
-    default = [
-        1,
-        5,
-        10,
-    ]
+resource "aws_sqs_queue" "user_updates_queue" {
+  name   = "user-updates-queue"
+  policy = data.aws_iam_policy_document.sqs_queue_policy.json
 }
 
-resource "aws_sns_topic" "billing" {
-    name = "BillingAlarm"
+resource "aws_sns_topic_subscription" "user_updates_queue" {
+  topic_arn = aws_sns_topic.user_updates.arn
+  protocol  = "email"
+  endpoint  = var.private.email
 }
 
-resource "aws_cloudwatch_metric_alarm" "billing" {
-    alarm_name = "${var.billing_name} lv.${count.index + 1} (${var.billing_threshold[count.index]} USD)"
-    namespace = "AWS/Billing"
-    metric_name = "EstimatedCharges"
-    statistic = "Maximum"
-    evaluation_periods = "1"
-    comparison_operator = "GreaterThanOrEqualToThreshold"
-    period = "21600"
-    threshold = "${var.billing_threshold[count.index]}"
-    alarm_description= "Total Charge ${var.billing_threshold[count.index]} USD"
-    alarm_actions = [ "${aws_sns_topic.billing.arn}" ]
-    count = "${length(var.billing_threshold)}"
+data "aws_iam_policy_document" "sqs_queue_policy" {
+  policy_id = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
 
-    dimensions = {
-        currency = "USD"
+  statement {
+    sid    = "user_updates_sqs_target"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["sns.amazonaws.com"]
     }
+
+    actions = [
+      "SQS:SendMessage",
+    ]
+
+    resources = [
+      "arn:aws:iam::aws:policy/AmazonSQSFullAccess",
+    ]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+
+      values = [
+        aws_sns_topic.user_updates.arn,
+      ]
+    }
+  }
+}
+
+resource "aws_budgets_budget" "cost" {
+  name              = "you-are-running-out-of-money"
+  budget_type       = "COST"
+  limit_amount      = "5"
+  limit_unit        = "USD"
+  time_period_end   = "2087-06-15_00:00"
+  time_period_start = "2017-07-01_00:00"
+  time_unit         = "MONTHLY"
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 5
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = [var.private.email]
+    subscriber_sns_topic_arns  = [var.sns.subscriber-sns-topic-arns]
+  }
 }
