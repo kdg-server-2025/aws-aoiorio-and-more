f6830e5177b4603181bb025c312729017b69545f
aoiorio <130330538+aoiorio@users.noreply.github.com>
2025-06-21 10:56:42 +0900

    wip: creating lambda environment

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+aws_ecr_authorization_token = { username = "aoiorio", password = "Atomudabe071@" }
+notification_email          = "aoi.orio0118@gmail.com"
+aws_caller_account_id       = "381492266241"
--- versions.tf
+++ versions.tf
@@ -20,3 +20,16 @@ terraform {
 provider "aws" {
   region  = "ap-northeast-1"
 }
+
+# data "aws_ecr_authorization_token" "token" {
+#   user_name = ""
+#   password = ""
+# }
+
+provider "docker" {
+  registry_auth {
+    address  = "${var.aws_caller_identity}.dkr.ecr.ap-northeast-1.amazonaws.com"
+    username = var.aws_ecr_authorization_token.token.user_name
+    password = var.aws_ecr_authorization_token.token.password
+  }
+}
