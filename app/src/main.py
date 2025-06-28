feedb679594963a1e4ea9366c43be940cb71f31f
aoiorio <130330538+aoiorio@users.noreply.github.com>
2025-06-28 14:47:58 +0900

    feat(lambda): create a lambda function using terraform and deploy processes (go ver)

--- app/src/main.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-
-def handler(*args, **kwargs):
-    res = requests.get('https://api.github.com')
-    return res.status_code
--- lambda.tf
+++ lambda.tf
@@ -8,22 +8,37 @@ resource "aws_s3_bucket" "aoiorio-bucket-lambda-artifacts" {
 
 # ロールを生成
 resource "aws_iam_role" "lambda" {
-  name               = "iam_for_lambda"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
+  name = "iam_for_lambda"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Effect = "Allow",
+        Sid    = ""
+      }
+    ]
+  })
 }
-EOF
+
+# GetAccountSettings の権限をインラインポリシーとして付与
+resource "aws_iam_role_policy" "get_account_settings" {
+  name = "GetAccountSettingsPermission"
+  role = aws_iam_role.lambda.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = "lambda:GetAccountSettings"
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
 
 # 初回のみ利用する空のLambdaのファイルを生成
@@ -32,7 +47,7 @@ data "archive_file" "initial_lambda_package" {
   output_path = "${path.module}/.temp_files/lambda.zip"
   source {
     content  = "# empty"
-    filename = "main.py"
+    filename = "fuga.txt"
   }
 }
 
@@ -45,13 +60,19 @@ resource "aws_s3_object" "lambda_file" {
 
 # Lambda関数を生成
 # ソースコードは空のLambdaのファイルのS3を参照
-resource "aws_lambda_function" "lambda_test" {
-  function_name = "lambda_test"
+resource "aws_lambda_function" "hello_go_function" {
+  function_name = "hello_go_function"
   role          = aws_iam_role.lambda.arn
   handler       = "main.handler"
-  runtime       = "python3.8"
+  runtime       = "provided.al2023"
   timeout       = 120
   publish       = true
   s3_bucket     = aws_s3_bucket.aoiorio-bucket-lambda-artifacts.id
   s3_key        = aws_s3_object.lambda_file.id
+}
+
+# 外部からリクエストを飛ばすためのエンドポイント
+resource "aws_lambda_function_url" "hello_go_function" {
+  function_name      = aws_lambda_function.hello_go_function.function_name
+  authorization_type = "NONE"
 }
