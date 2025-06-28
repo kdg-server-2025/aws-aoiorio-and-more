76071a4853b82634cd5259ad793f428e83d2684d
aoiorio <130330538+aoiorio@users.noreply.github.com>
2025-06-28 13:55:59 +0900

    fix(file): make vpc and ec2 inactive

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_sns_topic" "budget_alarm" {
 resource "aws_sns_topic_subscription" "topic_subscription_atom" {
   topic_arn = aws_sns_topic.budget_alarm.arn
   protocol  = "email"
-  endpoint  = var.private.email
+  endpoint  = var.email
 }
 
 resource "aws_budgets_budget" "cost" {
@@ -22,78 +22,7 @@ resource "aws_budgets_budget" "cost" {
     threshold                  = 5
     threshold_type             = "PERCENTAGE"
     notification_type          = "FORECASTED"
-    subscriber_email_addresses = [var.notification_email]
+    subscriber_email_addresses = [var.email]
     subscriber_sns_topic_arns  = [aws_sns_topic.budget_alarm.arn]
   }
 }
-
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-# resource "aws_ecr_repository" "python_repo_for_lambda" {
-#   name                 = "python-repo-for-lambda"
-#   image_tag_mutability = "MUTABLE"
-
-#   image_scanning_configuration {
-#     scan_on_push = true
-#   }
-# }
-
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-module "lambda_function" {
-  source = "terraform-aws-modules/lambda/aws"
-
-  function_name  = "hello-lambda-api"
-  create_package = false
-
-  image_uri    = module.docker_image.image_uri
-  package_type = "Image"
-  memory_size = 10240
-  timeout = 15
-
-  depends_on = [module.docker_image]
-}
-
-module "docker_image" {
-  source = "terraform-aws-modules/lambda/aws//modules/docker-build"
-
-  create_ecr_repo = true
-  ecr_repo        = "hello-lambda-api-repo"
-
-  use_image_tag = true
-  image_tag     = "1.0"
-
-  triggers = {
-    redeployment = timestamp()
-  }
-
-  source_path = "./api"
-}
-
-# resource "aws_lambda_function" "hello_lambda_func" {
-#   function_name = "hello-lambda-func"
-#   role          = aws_iam_role.iam_for_lambda.arn
-#   runtime       = "python3.13"
-#   handler       = "app.handler"
-#   source_code_hash = data.archive_file.function_source.output_base64sha256
-#   package_type  = "Image"
-#   image_uri     = "${aws_ecr_repository.python_repo_for_lambda.repository_url}:latest"
-
-#   ephemeral_storage {
-#     size = 10240 # Min 512 MB and the Max 10240 MB
-#   }
-# }
--- terraform.tfvars
+++
@@ -1,3 +0,0 @@
-aws_ecr_authorization_token = { username = "aoiorio", password = "Atomudabe071@" }
-notification_email          = "aoi.orio0118@gmail.com"
-aws_caller_account_id       = "381492266241"
