cc50a987fd79bd7acd82a2cfa634f472d1a6e0e7
aoiorio <130330538+aoiorio@users.noreply.github.com>
2025-07-12 12:08:45 +0900

    delete(terraform): comment out lambda tf codes

--- lambda.tf
+++ lambda.tf
@@ -1,10 +1,10 @@
-resource "aws_s3_bucket" "aoiorio-bucket-lambda-artifacts" {
-  bucket = "aoiorio-bucket-lambda-artifacts"
-  tags = {
-    Name        = "aoiorio-lambda-bucket"
-    Environment = "Dev"
-  }
-}
+# resource "aws_s3_bucket" "aoiorio-bucket-lambda-artifacts" {
+#   bucket = "aoiorio-bucket-lambda-artifacts"
+#   tags = {
+#     Name        = "aoiorio-lambda-bucket"
+#     Environment = "Dev"
+#   }
+# }
 
 # ロールを生成
 resource "aws_iam_role" "lambda" {
@@ -41,38 +41,38 @@ resource "aws_iam_role_policy" "get_account_settings" {
   })
 }
 
-# 初回のみ利用する空のLambdaのファイルを生成
-data "archive_file" "initial_lambda_package" {
-  type        = "zip"
-  output_path = "${path.module}/.temp_files/lambda.zip"
-  source {
-    content  = "# empty"
-    filename = "fuga.txt"
-  }
-}
+# # 初回のみ利用する空のLambdaのファイルを生成
+# data "archive_file" "initial_lambda_package" {
+#   type        = "zip"
+#   output_path = "${path.module}/.temp_files/lambda.zip"
+#   source {
+#     content  = "# empty"
+#     filename = "fuga.txt"
+#   }
+# }
 
 # 生成した空のLambdaのファイルをS3にアップロード
-resource "aws_s3_object" "lambda_file" {
-  bucket = aws_s3_bucket.aoiorio-bucket-lambda-artifacts.id
-  key    = "initial.zip"
-  source = "${path.module}/.temp_files/lambda.zip"
-}
+# resource "aws_s3_object" "lambda_file" {
+#   bucket = aws_s3_bucket.aoiorio-bucket-lambda-artifacts.id
+#   key    = "initial.zip"
+#   source = "${path.module}/.temp_files/lambda.zip"
+# }
 
 # Lambda関数を生成
 # ソースコードは空のLambdaのファイルのS3を参照
-resource "aws_lambda_function" "hello_go_function" {
-  function_name = "hello_go_function"
-  role          = aws_iam_role.lambda.arn
-  handler       = "main.handler"
-  runtime       = "provided.al2023"
-  timeout       = 120
-  publish       = true
-  s3_bucket     = aws_s3_bucket.aoiorio-bucket-lambda-artifacts.id
-  s3_key        = aws_s3_object.lambda_file.id
-}
+# resource "aws_lambda_function" "hello_go_function" {
+#   function_name = "hello_go_function"
+#   role          = aws_iam_role.lambda.arn
+#   handler       = "main.handler"
+#   runtime       = "provided.al2023"
+#   timeout       = 120
+#   publish       = true
+#   s3_bucket     = aws_s3_bucket.aoiorio-bucket-lambda-artifacts.id
+#   s3_key        = aws_s3_object.lambda_file.id
+# }
 
 # 外部からリクエストを飛ばすためのエンドポイント
-resource "aws_lambda_function_url" "hello_go_function" {
-  function_name      = aws_lambda_function.hello_go_function.function_name
-  authorization_type = "NONE"
-}
+# resource "aws_lambda_function_url" "hello_go_function" {
+#   function_name      = aws_lambda_function.hello_go_function.function_name
+#   authorization_type = "NONE"
+# }
