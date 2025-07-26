a12e9ff2e874f215b90e6c1b2db09fa19b06a87d
aoiorio <130330538+aoiorio@users.noreply.github.com>
2025-07-26 11:50:34 +0900

    delete(lambda): comment out lines of codes to obliterate a function

--- lambda.tf
+++ lambda.tf
@@ -7,27 +7,27 @@
 # }
 
 # ロールを生成
-resource "aws_iam_role" "lambda" {
-  name = "iam_for_lambda"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        },
-        Effect = "Allow",
-        Sid    = ""
-      }
-    ]
-  })
-}
+# resource "aws_iam_role" "lambda" {
+#   name = "iam_for_lambda"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole",
+#         Principal = {
+#           Service = "lambda.amazonaws.com"
+#         },
+#         Effect = "Allow",
+#         Sid    = ""
+#       }
+#     ]
+#   })
+# }
 
 # GetAccountSettings の権限をインラインポリシーとして付与
 resource "aws_iam_role_policy" "get_account_settings" {
   name = "GetAccountSettingsPermission"
-  role = aws_iam_role.lambda.id
+  role = "iam_for_lambda"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -51,18 +51,18 @@ resource "aws_iam_role_policy" "get_account_settings" {
 #   }
 # }
 
-# 生成した空のLambdaのファイルをS3にアップロード
+# # 生成した空のLambdaのファイルをS3にアップロード
 # resource "aws_s3_object" "lambda_file" {
 #   bucket = aws_s3_bucket.aoiorio-bucket-lambda-artifacts.id
 #   key    = "initial.zip"
 #   source = "${path.module}/.temp_files/lambda.zip"
 # }
 
-# Lambda関数を生成
-# ソースコードは空のLambdaのファイルのS3を参照
+# # Lambda関数を生成
+# # ソースコードは空のLambdaのファイルのS3を参照
 # resource "aws_lambda_function" "hello_go_function" {
 #   function_name = "hello_go_function"
-#   role          = aws_iam_role.lambda.arn
+#   role          = var.iam_for_lambda_arn
 #   handler       = "main.handler"
 #   runtime       = "provided.al2023"
 #   timeout       = 120
@@ -71,7 +71,7 @@ resource "aws_iam_role_policy" "get_account_settings" {
 #   s3_key        = aws_s3_object.lambda_file.id
 # }
 
-# 外部からリクエストを飛ばすためのエンドポイント
+# # 外部からリクエストを飛ばすためのエンドポイント
 # resource "aws_lambda_function_url" "hello_go_function" {
 #   function_name      = aws_lambda_function.hello_go_function.function_name
 #   authorization_type = "NONE"
